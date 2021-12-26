b3473babecbf6440086e4ecba2556674116a5079
Louise Iyengar <louise.iyengar@flatironschool.com>
2021-12-26 01:11:41 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const feetPerBlock = 264
+
+function distanceFromHqInBlocks(pickUp) {
+  const HQLocation = 42
+  return Math.abs(pickUp - HQLocation)
+}
+
+function distanceFromHqInFeet(pickUp) {
+  const distance = distanceFromHqInBlocks(pickUp);
+  return distance * feetPerBlock;
+}
+
+function distanceTravelledInFeet(pickUp, dropOff) {
+  const distance = Math.abs(pickUp - dropOff);
+  return distance * feetPerBlock;
+}
+
+function calculatesFarePrice(pickUp, dropOff) {
+  //returns the fare for the customer
+  const distanceInFeet = distanceTravelledInFeet(pickUp, dropOff)
+  if (distanceInFeet <= 400) {
+    return 0  
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+    const cost = 0.02;
+    const chargedDistance = distanceInFeet - 400;
+    return cost * chargedDistance;
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return 25
+  } else {
+    return "cannot travel that far"
+  }
+}
